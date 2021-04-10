bdb5816e5112244d1a563884f2e93d617ad5d273
Sugar_F0x <sugarfox.dev@gmail.com>
2021-04-10 13:40:19 +0300

    :white_check_mark: Add Terms of Reference component
    > Component takes 'doc' prop that is the name of parent component
      * this name is used to fetch content to be displayed

---
+++ frontend/components/ToR.vue
@@ -0,0 +1,26 @@
+<template lang="pug">
+  nuxt-content(:document="document")
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: "TK",
+  props: {
+    doc: String
+  },
+  data() {
+    return {
+      document: null as any
+    }
+  },
+  async fetch() {
+    this.document = await this.$content(this.doc).fetch()
+  },
+})
+</script>
+
+<style lang="sass" scoped>
+
+</style>
