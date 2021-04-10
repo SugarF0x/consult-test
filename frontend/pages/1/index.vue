a6b656b16fc85f3b579d90aec262348d14056832
Sugar_F0x <sugarfox.dev@gmail.com>
2021-04-10 13:31:16 +0300

    :white_check_mark: Add balance() function

--- frontend/pages/1/index.vue
+++ frontend/pages/1/index.vue
@@ -6,7 +6,33 @@
 import Vue from 'vue'
 
 export default Vue.extend({
-  name: "task-1"
+  name: "task-1",
+  methods: {
+    balance(left: string, right: string) {
+      const EXCLAIM_WEIGHT = 2
+      const QUESTION_WEIGHT = 3
+
+      function calculateWeight(str: string): number {
+        const exclaimsWeight = (str.match(/!/g)?.length || 0) * EXCLAIM_WEIGHT
+        const questionWeight = (str.match(/\?/g)?.length || 0) * QUESTION_WEIGHT
+
+        return exclaimsWeight + questionWeight
+      }
+
+      const leftWeight = calculateWeight(left)
+      const rightWeight = calculateWeight(right)
+
+      if (leftWeight > rightWeight) return 'Left'
+      if (leftWeight < rightWeight) return 'Right'
+      return 'Balance'
+    }
+  },
+  mounted() {
+    console.log(this.balance("!!","??")) // Right
+    console.log(this.balance("!??","?!!")) // Left
+    console.log(this.balance("!?!!","?!?")) // Left
+    console.log(this.balance("!!???!????","??!!?!!!!!!!")) // Balance
+  }
 })
 </script>
 
